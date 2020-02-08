b3a9771d1e3dbb2a437e990a6febda50f4e42780
Khalid Hosein <khalid.hosein9@gmail.com>
2020-02-08 15:34:53 -0500

    Updates

---
+++ precache-manifest.06af1d03cac7ddc36286a7965004f83f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e87cfaa77355d88f9433d9f5d2929d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "6c6364d28de063c75c5e",
+    "url": "/my-app/static/css/main.4ca26bdb.chunk.css"
+  },
+  {
+    "revision": "d53b286980e3a5612509",
+    "url": "/my-app/static/js/2.b4d228fe.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/my-app/static/js/2.b4d228fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c6364d28de063c75c5e",
+    "url": "/my-app/static/js/main.36f7c8b5.chunk.js"
+  },
+  {
+    "revision": "6724d87984303fc7dd0e",
+    "url": "/my-app/static/js/runtime-main.974732b3.js"
+  },
+  {
+    "revision": "e3553f169d4581e7e1bec7b3c39185f0",
+    "url": "/my-app/static/media/beatboxscreen.e3553f16.png"
+  },
+  {
+    "revision": "b9fb9eb5670cb1e8a4053e61c119003d",
+    "url": "/my-app/static/media/cbdscreen.b9fb9eb5.jpg"
+  },
+  {
+    "revision": "53d877e199bde8a9d5b8166c72dc7040",
+    "url": "/my-app/static/media/goatscreen.53d877e1.png"
+  },
+  {
+    "revision": "6429a88fbfcf0729b86193c8911cdbe5",
+    "url": "/my-app/static/media/marblescreen.6429a88f.jpg"
+  },
+  {
+    "revision": "c49b63ed5b80951be8af168872d75877",
+    "url": "/my-app/static/media/mercerscreen.c49b63ed.jpg"
+  },
+  {
+    "revision": "97ac0621f68e5783c637d8731e63e2a1",
+    "url": "/my-app/static/media/praizorscreen.97ac0621.jpg"
+  },
+  {
+    "revision": "a5dbca49403047bdb382fd8169709c4c",
+    "url": "/my-app/static/media/travelbudzscreen.a5dbca49.png"
+  },
+  {
+    "revision": "2b18260ac2e2d628603910fb3f326a8a",
+    "url": "/my-app/static/media/travelscreen.2b18260a.jpg"
+  }
+]);
